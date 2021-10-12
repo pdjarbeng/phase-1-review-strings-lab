f9514ac9f42ce76f415d94bf8daf838b374f408f
priscilla <pdjarbeng@outlook.com>
2021-10-12 12:23:31 -0400

    commit tree 714e218bdc4e5371d988149f55e59cc73a92a1dc

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+const currentUser = 'Priscilla Djarbeng'
+////const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;  <-same as line 10
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+//const shortGreeting = 'Welcome, ${currentUser.slice(0, 1)}!'
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
